2fc7e1aec8ebf08f4c6907846b6b8aae6bd4344b
Phuwadon Verasertniyom <phuwadonpeak@hotmail.com>
2026-08-31 23:50:20 +0700

    Upgrade pacman before installing packages

--- install.sh
+++ install.sh
@@ -8,9 +8,11 @@ OS_ID_LIKE="$ID_LIKE"
 
 case "$OS_ID_LIKE" in
     'cygwin arch')
+        pacman -Syu --noconfirm
         pacman --needed --noconfirm -S nasm mingw-w64-ucrt-x86_64-qemu unzip
         printf '\nPress any key to continue\n'
         read -rs -n 1 ;;
     'arch')
+        sudo pacman -Syu --noconfirm
         sudo pacman --needed --noconfirm -S nasm qemu unzip ;;
 esac
