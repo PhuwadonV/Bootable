42071fa21b88acd6d36af5cb07f5d5f72deb5322
Phuwadon Verasertniyom <phuwadonpeak@hotmail.com>
2026-09-01 12:01:56 +0700

    Fix package dependency issues by combining pacman upgrade and installation

--- install.sh
+++ install.sh
@@ -8,11 +8,9 @@ OS_ID_LIKE="$ID_LIKE"
 
 case "$OS_ID_LIKE" in
     'cygwin arch')
-        pacman -Syu --noconfirm
-        pacman --needed --noconfirm -S nasm mingw-w64-ucrt-x86_64-qemu unzip
+        pacman --needed --noconfirm -Syu nasm mingw-w64-ucrt-x86_64-qemu unzip
         printf '\nPress any key to continue\n'
         read -rs -n 1 ;;
     'arch')
-        sudo pacman -Syu --noconfirm
-        sudo pacman --needed --noconfirm -S nasm qemu unzip ;;
+        sudo pacman --needed --noconfirm -Syu nasm qemu unzip ;;
 esac
