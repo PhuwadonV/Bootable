d86cb2312b33db5573998790bb8a12dbe0e51ec0
Phuwadon Verasertniyom <phuwadonpeak@hotmail.com>
2026-09-08 00:27:23 +0700

    Reorder pacman installation flags

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ OS_ID_LIKE="$ID_LIKE"
 
 case "$OS_ID_LIKE" in
     'cygwin arch')
-        pacman --needed --noconfirm -Syu nasm mingw-w64-ucrt-x86_64-qemu unzip
+        pacman -Syu --needed --noconfirm nasm mingw-w64-ucrt-x86_64-qemu unzip
         printf '\nPress any key to continue\n'
         read -rs -n 1 ;;
     'arch')
-        sudo pacman --needed --noconfirm -Syu nasm qemu unzip ;;
+        sudo pacman -Syu --needed --noconfirm nasm qemu unzip ;;
 esac
